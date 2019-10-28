616d0e413159df1caab290f7952d8829f501472f
b.egorov <b.egorov@tinkoff.ru>
2019-10-29 00:32:01 +0300

    Get nodes

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,20 @@
 module Main where
 
 import Control.Monad (foldM)
-import Data.List (zip, filter)
+import Data.List (zipWith, filter, map, sort)
+import Data.HashMap (fromList, Map)
+import Data.String (words)
 
 main :: IO ()
 main = do
   s <- getLine
-  let n = (read s :: Int)
+  let n = read s :: Int
   ss <- foldM readInput [] [1..n]
-  let leaves = map snd $ filter ((==) "0" . fst . fst) $ zip (reverse ss) [1..]
+  let indexed = zipWith (\a b -> (a, toNode b)) [1..] (reverse ss)
+  let leaves = sort $ map fst $ filter (isLeave . snd) indexed
+  let nodes = fromList indexed :: Map Int T
   putStrLn $ show leaves
+  putStrLn $ show nodes
 
 readInput :: [(String, String)] -> Int -> IO [(String, String)]
 readInput ss _ = do
@@ -21,4 +26,14 @@ readInput ss _ = do
   else
     getLine >>= \s' -> return $ (s, s'):ss
 
---data T =
+data T = L Bool | N [Int] [Bool] deriving Show
+
+isLeave :: T -> Bool
+isLeave (L _) = True
+isLeave _     = False
+
+toNode :: (String, String) -> T
+toNode ("0", _) = L False
+toNode (a, b)   = N
+  (map (\s -> read s :: Int) $ tail $ words a)
+  (map (== "1") $ words b)
