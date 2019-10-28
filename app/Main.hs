58a9a1d3b0f97e4c8a7e368506aea3f6581ea822
b.egorov <b.egorov@tinkoff.ru>
2019-10-28 23:16:41 +0300

    Read input

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
 module Main where
 
-import Lib
+import Control.Monad (foldM)
 
 main :: IO ()
-main = someFunc
+main = do
+  s <- getLine
+  let n = (read s :: Int)
+  ss <- foldM readInput [] [1..n]
+  putStrLn $ show ss
+
+readInput :: [String] -> Int -> IO [String]
+readInput ss _ = do
+  s <- getLine
+  if
+    s == "0"
+  then
+    return $ s:ss
+  else
+    getLine >>= \s' -> return $ s':s:ss
+
+--data T =
