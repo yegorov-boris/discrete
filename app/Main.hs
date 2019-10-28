9da38c0bb85ab1e0d71f42f6a71434bc4bd225ea
b.egorov <b.egorov@tinkoff.ru>
2019-10-28 23:33:56 +0300

    Get leaves indexes

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,24 @@
 module Main where
 
 import Control.Monad (foldM)
+import Data.List (zip, filter)
 
 main :: IO ()
 main = do
   s <- getLine
   let n = (read s :: Int)
   ss <- foldM readInput [] [1..n]
-  putStrLn $ show ss
+  let leaves = map snd $ filter ((==) "0" . fst . fst) $ zip (reverse ss) [1..]
+  putStrLn $ show leaves
 
-readInput :: [String] -> Int -> IO [String]
+readInput :: [(String, String)] -> Int -> IO [(String, String)]
 readInput ss _ = do
   s <- getLine
   if
     s == "0"
   then
-    return $ s:ss
+    return $ (s, ""):ss
   else
-    getLine >>= \s' -> return $ s':s:ss
+    getLine >>= \s' -> return $ (s, s'):ss
 
 --data T =
